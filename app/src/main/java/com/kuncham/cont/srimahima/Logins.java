daae4d153e895c2e4a9bf655cc553a41412ffd00
admin <cdastagirireddy@hotmail.com>
2018-02-26 11:30:01 +0530

    activities

---
+++ app/src/main/java/com/kuncham/cont/srimahima/Logins.java
@@ -0,0 +1,13 @@
+package com.kuncham.cont.srimahima;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Logins extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logins);
+    }
+}
